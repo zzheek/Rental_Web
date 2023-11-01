0f16f51faffff4ad7e1c5a662c299d92cd5d7a57
malvin222 <mahebin@naver.com>
2023-11-01 11:03:54 +0900

    first

---
+++ src/main/java/com/rental_web/RentalWebApplication.java
@@ -0,0 +1,13 @@
+package com.rental_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental_web/RentalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
