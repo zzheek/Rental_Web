674b56dcf6504bf0d8aeac932fe516127d93572b
Haebin <mahebin@naver.com>
2023-11-02 12:44:02 +0900

    BaseEntity.java 생성

---
+++ src/main/java/com/rental_web/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.rental_web.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "csdate", updatable = false)
+    private LocalDateTime csdate;
+
+    @CreatedDate
+    @Column(name="csredate", updatable = false)
+    private LocalDateTime csredate;
+
+}
