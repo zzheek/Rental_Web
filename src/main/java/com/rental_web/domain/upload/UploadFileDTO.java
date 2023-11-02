1b1b56dcc686b1fb40b8a2f6a196daf7ccbcfb41
Haebin <mahebin@naver.com>
2023-11-02 17:10:02 +0900

    Product 게시판 CRUD - Register 화면 구현 ..ing

---
+++ src/main/java/com/rental_web/domain/upload/UploadFileDTO.java
@@ -0,0 +1,12 @@
+package com.rental_web.domain.upload;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+
+    private List<MultipartFile> files;
+}
