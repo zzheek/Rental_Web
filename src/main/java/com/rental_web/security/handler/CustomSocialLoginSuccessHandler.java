14d7c6391c7606381025f6d9617f74ce360671a6
zzheek <zzjuheek@gmail.com>
2023-11-09 17:29:52 +0900

    Renboard list 수정 nav/sidebar 수정

---
+++ src/main/java/com/rental_web/security/handler/CustomSocialLoginSuccessHandler.java
@@ -0,0 +1,49 @@
+package com.rental_web.security.handler;
+
+import com.rental_web.security.dto.MemberSecurityDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSocialLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        log.info("----------------------------------------------------------");
+        log.info("CustomLoginSuccessHandler onAuthenticationSuccess ..........");
+        log.info(authentication.getPrincipal());
+
+        MemberSecurityDTO memberSecurityDTO = (MemberSecurityDTO) authentication.getPrincipal();
+
+        String encodedPw = memberSecurityDTO.getMemberpass();
+
+        //소셜로그인이고 회원의 패스워드가 1111이라면
+        if (memberSecurityDTO.isSocial()
+                && (memberSecurityDTO.getMemberpass().equals("1111")
+                ||  passwordEncoder.matches("1111", memberSecurityDTO.getMemberpass())
+        )) {
+            log.info("Should Change Password");
+
+            log.info("Redirect to Member Modify ");
+            response.sendRedirect("/member/modify");
+
+            return;
+        } else {
+
+            response.sendRedirect("/");
+        }
+    }
+}
